020d96216731fcf648fdd0499c1df580d3b149bd
Fernanda Santos <sfernanda1@outlook.com>
2021-10-07 19:46:58 -0300

    first commit

---
+++ src/componentes/Primeiro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ export default function  () {
+    return (
+        <div>  
+        <h1>Primeiro Componente</h1>
+        <h2>Exemplo de um componente react</h2>
+        </div>
+    )
+}
+// Não é possível retornar dois elementos jsx adjascente. 
+//Para isso é preciso usar uma div, ou <>, ou até <React.Fragment>
